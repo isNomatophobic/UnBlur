bc889e0fda848668a040d46f03be451089702771
kristian diez <isnomatophobic@gmail.com>
2023-09-07 20:39:55 +0300

    Initial script

---
+++ scripts/unblur.js
@@ -0,0 +1,18 @@
+setTimeout(() => {
+  const allElementsArray = document.getElementsByTagName("*");
+  console.log(allElementsArray);
+  for (let i = 0; i < allElementsArray.length; i += 1) {
+    const currentElement = allElementsArray[i];
+    if (currentElement.style) {
+      const filter = currentElement.style.filter;
+      const blurIndex = filter.indexOf("blur");
+      console.log(filter);
+      if (!filter || blurIndex === -1) continue;
+      console.log([currentElement]);
+      currentElement.style.filter = filter.replace(
+        filter.slice(4, filter.indexOf(")") + 1),
+        ""
+      );
+    }
+  }
+}, 1000);
