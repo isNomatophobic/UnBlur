4937c7d2067405dedb6f4ceb2e094e88a3e53634
kristian diez <isnomatophobic@gmail.com>
2023-09-10 21:17:38 +0300

    Get computed styles and fix filter func

--- scripts/unblur.js
+++ scripts/unblur.js
@@ -5,13 +5,15 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   for (let i = 0; i < allElementsArray.length; i += 1) {
     const currentElement = allElementsArray[i];
     if (currentElement.style) {
-      const filter = currentElement.style.filter;
+      const computed = getComputedStyle(currentElement);
+      const filter = computed.getPropertyValue("filter");
       const blurIndex = filter.indexOf("blur");
-      console.log(filter);
+
       if (!filter || blurIndex === -1) continue;
       console.log([currentElement]);
+
       currentElement.style.filter = filter.replace(
-        filter.slice(4, filter.indexOf(")") + 1),
+        filter.slice(blurIndex, filter.indexOf(")") + 1),
         ""
       );
     }
