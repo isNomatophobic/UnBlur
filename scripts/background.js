820529aaceceb98530b1040062caf16c26cf1d07
kristian diez <isnomatophobic@gmail.com>
2023-09-15 23:12:43 +0300

    Test

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,10 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.insertCSS({
-    target: { tabId: tab.id },
-    files: ["assets/alert.css"],
-  });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ["scripts/unblur.js"],
+  });
+  chrome.scripting.insertCSS({
+    target: { tabId: tab.id },
+    files: ["assets/alert.css"],
   });
 });
